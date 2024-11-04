3ac692bde05fdd451b777beb3ce3f1efc9e0690c
code <code_lioko1@hotmail.com>
2024-11-03 23:07:11 -0600

    Enhance model evaluation by adding run ID validation and error handling in MLflow integration

--- src/evaluate.py
+++ src/evaluate.py
@@ -6,28 +6,37 @@ import os
 import mlflow
 
 def evaluate_model(model_path, X_test_path, y_test_path, output_path, run_id):
-    model = joblib.load(model_path)
-    X_test = pd.read_csv(X_test_path)
-    y_test = pd.read_csv(y_test_path)
+    mlflow.set_tracking_uri("http://127.0.0.1:5000")  # Ajusta esto si es necesario
+    try:
+        # Verificar si el run_id existe
+        run = mlflow.get_run(run_id)
+        if run is None or run.info.status == "FINISHED":
+            raise mlflow.exceptions.MlflowException(f"Run '{run_id}' not found or already finished.")
 
-    predictions = model.predict(X_test)
+        model = joblib.load(model_path)
+        X_test = pd.read_csv(X_test_path)
+        y_test = pd.read_csv(y_test_path)
 
-    report = classification_report(y_test, predictions, zero_division=1)
-    cm = confusion_matrix(y_test, predictions)
+        predictions = model.predict(X_test)
 
-    with mlflow.start_run(run_id=run_id):
-        accuracy = accuracy_score(y_test, predictions)
-        mlflow.log_metric("accuracy", accuracy)
+        report = classification_report(y_test, predictions, zero_division=1)
+        cm = confusion_matrix(y_test, predictions)
 
-        report_dict = classification_report(y_test, predictions, output_dict=True)
+        with mlflow.start_run(run_id=run_id):
+            accuracy = accuracy_score(y_test, predictions)
+            mlflow.log_metric("accuracy", accuracy)
+
+            report_dict = classification_report(y_test, predictions, output_dict=True)
+            
+            mlflow.log_metric("precision", report_dict['weighted avg']['precision'])
+            mlflow.log_metric("recall", report_dict['weighted avg']['recall'])
+            mlflow.log_metric("f1-score", report_dict['weighted avg']['f1-score'])
+            
+            mlflow.log_dict({"confusion_matrix": cm.tolist()}, "confusion_matrix.json")
         
-        mlflow.log_metric("precision", report_dict['weighted avg']['precision'])
-        mlflow.log_metric("recall", report_dict['weighted avg']['recall'])
-        mlflow.log_metric("f1-score", report_dict['weighted avg']['f1-score'])
-        
-        mlflow.log_dict({"confusion_matrix": cm.tolist()}, "confusion_matrix.json")
-    
-    write_evaluation_report(output_path, report, cm)
+        write_evaluation_report(output_path, report, cm)
+    except mlflow.exceptions.MlflowException as e:
+        print(f"Error: {e}")
 
 def write_evaluation_report(file_path, report, confusion_matrix):
     os.makedirs(os.path.dirname(file_path), exist_ok=True)
